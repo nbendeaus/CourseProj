ca5eca9c1ef49c287011a46f8bed06dbda827ae0
Vineeta Khurana <vineeta.khuran27@gmail.com>
2018-09-29 17:33:59 -0400

    adding basic content to components per app planning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { HeaderComponent } from './header/header.component';
 import { ReceipesComponent } from './receipes/receipes.component';
 import { ReceipeListComponent } from './receipes/receipe-list/receipe-list.component';
 import { ReceipeDetailComponent } from './receipes/receipe-detail/receipe-detail.component';
-import { ReceipeItemComponent } from './receipe-list/receipe-item/receipe-item.component';
+import { ReceipeItemComponent } from './receipes/receipe-list/receipe-item/receipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
